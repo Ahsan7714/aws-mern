4286219bb54a326ee1bd8636fd8162c33d24270b
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-11-14 11:14:28 +0500

    .

--- Gezonfit50+/src/store/baseUrl.js
+++ Gezonfit50+/src/store/baseUrl.js
@@ -1,6 +1,9 @@
 
-const baseurl = "http://13.60.243.194:3000/api/v1"
-const socketBaseurl="ws://13.60.243.194:3000"
+// const baseurl = "http://13.60.243.194:3000/api/v1"
+// const socketBaseurl="ws://13.60.243.194:3000"
+
+const baseurl = "http://localhost:3000/api/v1"
+const socketBaseurl="ws://localhost:3000"
 
 export default baseurl
 export {socketBaseurl}
